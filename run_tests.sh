279b9ab89156948e5de184fb1e50aeb7294ea9e0
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-05-11 00:05:43 -0400

    clean up travis consul

--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-./consul agent -server -bootstrap-expect 1 -data-dir /tmp/consul &
-sleep 10  # sleep while it starts up
-./config/setup_consul_travis.sh
-curl http://localhost:8500/v1/kv/adi-website\?recurse
-nosetests
