aa2687fff1ecc92f51c0b7647f006d70acc90286
Ben Low <benlowkh@gmail.com>
2015-09-22 21:33:17 -0400

    added foundry route and html file

--- app/routes/client.py
+++ app/routes/client.py
@@ -89,6 +89,15 @@ def feedback():
     """
     return render_template('feedback.html')
 
+@client.route('/foundry', methods=['GET'])
+def foundry():
+    '''View information about ADI foundry
+    **Route:** ``/foundry``
+
+    **Methods:** ``GET``
+    '''
+
+    return render_template('foundry.html')
 
 @client.route('/jobfair', methods=['GET'])
 def jobfair():
